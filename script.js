0426241c4be8be4e02da5bcbdcfd5b0f22592f8d
Joachim MASSON <joachim.masson.17@gmail.com>
2025-10-02 14:48:25 +0200

    Add compteur in script.js and code in html

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 for (let i=0; i<10; i++){
    console.log("Number "+i);
 }
+
+const animals = ["😺", "🐶","🐭", "🦊", "🐻", "🐼"];
+for (let i =0; i<animals.length; i++){
+   console.log(animals[i]);
+}
+
+let i = 10;
+while (i > -1) {
+   console.log("number "+i);
+   i--;
+}
